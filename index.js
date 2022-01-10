cc2cb47f2e7cce607e827a9627d97a1be52f8556
Jimmy <dm.a.uvarov@gmail.com>
2022-01-10 16:52:57 -0500

    commit tree e6bba0e9cbcd219538915ee2c26ece8a7d09fe73

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+// 1
+let currentUser = "currentUser";
+
+//2
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+//3
+const excitedWelcomeMessage = welcomeMessage.toUpperCase() ;
+
+//4
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
